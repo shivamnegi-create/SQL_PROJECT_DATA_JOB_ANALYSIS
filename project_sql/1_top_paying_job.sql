4187955753b7d24802d36a116b5f5e3aa7a6995e
Shivam Negi <shivamnegixd@gmail.com>
2026-04-25 16:33:47 +0530

    first commit

---
+++ project_sql/1_top_paying_job.sql
@@ -0,0 +1,22 @@
+/* About the Project
+
+Goal
+
+1. You are an aspiring data nerd looking to analyze the top-paying roles and skills
+
+2. You will create SQL queries to explore this large dataset specific to you
+
+3. For those job searching or looking for a promotion; you can not only use this project
+ to showcase experience BUT also to extract what roles/skills you should target
+ Questions to Answer
+
+1. What are the top-paying jobs for my role?
+
+2. What are the skills required for these top-paying roles?
+
+3. What are the most in-demand skills for my role?
+
+4. What are the top skills based on salary for my role?
+
+5. What are the most optimal skills to learn?
+   • Optimal: High Demand AND High Paying */
